623ae46a14d952ffe4890f0ea64a7c2e56320f21
Yana Polianskaia <yanapolianskaiaa@gmail.com>
2023-07-12 16:13:42 +0300

    Created views, models, set up media

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import PostImage
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'insta/index.html'
+    context_object_name = 'object_list'
+
+class CreationDetailView(generic.DetailView):
+    model = PostImage
+    template_name = "insta/creation.html"
+
+class ResultDetailView(generic.DetailView):
+    model = PostImage
+    template_name = "insta/result.html"
+
+class DetailView(generic.DetailView):
+    model = PostImage
+    template_name = "insta/detail.html"
