dfc620d872443450d68ddcefcdcc66478570df16
Yana Polianskaia <yanapolianskaiaa@gmail.com>
2023-07-13 15:28:14 +0300

    many many some changes

--- insta/models.py
+++ insta/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class PostImage(models.Model):
     def __str__(self):
-        return self.image_source
+        return self.comment
     
-    image_source = models.ImageField(upload_to='images/', height_field=400, width_field=400, max_length=100)
+    image_source = models.ImageField(upload_to='images/', max_length=100)
     comment = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     likes = models.IntegerField(default=0)
--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 
@@ -6,8 +8,13 @@ from .models import PostImage
 # Create your views here.
 
 class IndexView(generic.ListView):
-    template_name = 'insta/index.html'
+    template_name = 'insta/pages/index.html'
     context_object_name = 'object_list'
+    # context['posts_count'] = models.PostImage.objects.count()
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return PostImage.objects.order_by('-pub_date')
 
 class SubmitView(generic.DetailView):
     model = PostImage
