dce15c4feb80505cd72524fdcb2704bcbabf6093
Acksel <egortrifo@gmail.com>
2023-07-14 00:33:37 +0300

    Очень много добавлений и изменений
    - Добавил формы
    - Сделал во views.py страницы index и submit

--- insta/urls.py
+++ insta/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from . import views
+#from insta.views import home_page
 
 app_name = 'insta'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('submit/', views.SubmitView.as_view(), name='submit'),
+    path('submit/', views.submit, name='submit'),
     path('result/<int:pk>', views.ResultView.as_view(), name='result'),
     path('detail/<int:pk>', views.DetailView.as_view(), name='detail'),
 ]
