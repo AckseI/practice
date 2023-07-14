b102c76616702a81e51ae8cd1b699e0063881ed1
Yana Polianskaia <yanapolianskaiaa@gmail.com>
2023-07-14 14:14:43 +0300

    Добавил стили к формочкам, добавил папку static

--- insta/forms.py
+++ insta/forms.py
@@ -6,4 +6,12 @@ class PostImageForm(ModelForm):
     class Meta:
         model = PostImage
         fields = ('image_source', 'comment')
+        labels = {
+            'image_source': 'Изображение',
+            'comment': 'Комментарий',
+        }
+        widgets = {
+            'image_source': forms.FileInput(attrs={'class':"form-control", 'type':"file", 'id':"image", 'name':"image"}),
+            'comment': forms.Textarea(attrs={'class':'form-control comment-field'}),
+        }
         
--- practice/settings.py
+++ practice/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -130,7 +132,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFIELS_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
